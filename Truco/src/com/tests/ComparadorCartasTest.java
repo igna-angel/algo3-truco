66264ef4e6b4e168560c6bd77119a76c777f5218
Ignacio Angel <ignacio.angel@hotmail.es>
2015-11-09 22:10:48 -0300

    Agregado comparador de cartas para calcular el truco

---
+++ Truco/src/com/tests/ComparadorCartasTest.java
@@ -0,0 +1,65 @@
+package com.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.modelo.Carta;
+import com.modelo.Carta.Palo;
+import com.modelo.ComparadorCartas;
+
+
+public class ComparadorCartasTest {
+	private Carta _carta1;
+	private Carta _carta2;
+	private Carta _carta3;
+	private Carta _carta4;
+	private Carta _carta5;
+	private Carta _carta6;
+	
+	@Before
+	public void setup(){
+		this._carta1 = new Carta(Palo.Espada, 1);
+		this._carta2 = new Carta(Palo.Basto, 1);
+		this._carta3 = new Carta(Palo.Espada, 7);
+		this._carta4 = new Carta(Palo.Copa, 7);
+		this._carta5 = new Carta(Palo.Oro, 4);
+		this._carta6 = new Carta(Palo.Copa, 4);
+	}
+	
+	@Test
+	public void testComparacionSinCartas(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(new ArrayList<Carta>()), -1);
+	}
+	
+	@Test
+	public void testComparacionUnaSolaCarta(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta1)), 0);
+	}
+	
+	@Test
+	public void testComparacionMismaCarta(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta1, this._carta1)), -1);
+	}
+	
+	@Test
+	public void testComparacionCartasMismoPalo(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta1, this._carta3)), 0);
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta4, this._carta6)), 0);
+	}
+	
+	@Test
+	public void testComparacionCartasMismoNumero(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta1, this._carta2)), 0);
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta5, this._carta6)), -1);
+	}
+	
+	@Test
+	public void testComparacionMuchasCartas(){
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta1, this._carta2, this._carta3, this._carta4, this._carta5, this._carta6)), 0);
+		Assert.assertEquals(ComparadorCartas.compararCartas(Arrays.asList(this._carta5, this._carta2, this._carta3, this._carta4, this._carta1, this._carta6)), 4);
+	}
+}
