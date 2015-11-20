805d0a5bbf08eaabe517eec7d44c93812f5b4e7c
FabrizioCozza <fabrizio.cozza@gmail.com>
2015-11-19 21:02:07 -0300

    Agregado el comparador para el proyecto real
    Hice un solo test por ahora pero ahora en un rato agrego mas, me falta
    interpretar donde estaria cada carta

--- Copy of Truco/src/com/modelo/ComparadorCartas.java
+++ Copy of Truco/src/com/modelo/ComparadorCartas.java
@@ -6,62 +6,18 @@ public class ComparadorCartas {
 	
 	public Carta compararCartas (Carta... cartas) {
 		
-		Carta cartaGanadora = null;
+		int cantidadDeCartas = cartas.length;
 		
-		if (cartas.length == 2){
-			cartaGanadora = compararCartas(2,cartas);
-		}
-		
-		if (cartas.length == 4){
-			cartaGanadora = compararCartas(4,cartas);
-		}
-		
-		if (cartas.length == 6){
-			cartaGanadora = compararCartas(6,cartas);
-		}
-		
-		return cartaGanadora;
+		return compararCartas(cantidadDeCartas,cartas);
 	}
 	
 	private Carta compararCartas(int cantidadDeCartas,Carta... cartas){
 		
-		int index = 0;
-		Carta cartaGanadora = null;
+		int primeraCarta = 0;
+		Carta cartaGanadora = cartas[primeraCarta];
 		
-		if (cantidadDeCartas == 2){
-			Carta primeraCarta = cartas[index];
-			Carta segundaCarta = cartas[index+1];
-			
-			cartaGanadora = primeraCarta.vs(segundaCarta);
-			return cartaGanadora;
-		}
-		
-		if (cantidadDeCartas == 4){
-			Carta primeraCarta = cartas[index];
-			Carta segundaCarta = cartas[index+1];
-			Carta terceraCarta = cartas[index+2];
-			Carta cuartaCarta = cartas[index+3];
-			
-			cartaGanadora = primeraCarta.vs(segundaCarta);
-			cartaGanadora = cartaGanadora.vs(terceraCarta);
-			cartaGanadora = cartaGanadora.vs(cuartaCarta);
-			return cartaGanadora;
-		}
-		
-		if (cantidadDeCartas == 6){
-			Carta primeraCarta = cartas[index];
-			Carta segundaCarta = cartas[index+1];
-			Carta terceraCarta = cartas[index+2];
-			Carta cuartaCarta = cartas[index+3];
-			Carta quintaCarta = cartas[index+4];
-			Carta sextaCarta = cartas[index+5];
-			
-			cartaGanadora = primeraCarta.vs(segundaCarta);
-			cartaGanadora = cartaGanadora.vs(terceraCarta);
-			cartaGanadora = cartaGanadora.vs(cuartaCarta);
-			cartaGanadora = cartaGanadora.vs(quintaCarta);
-			cartaGanadora = cartaGanadora.vs(sextaCarta);
-			return cartaGanadora;
+		for (int i=1; i < cantidadDeCartas ; i++){
+			cartaGanadora = cartaGanadora.vs(cartas[i]);
 		}
 		
 		return cartaGanadora; //podria haber una cantidad de cartas incorrecta, ver excepcion 
