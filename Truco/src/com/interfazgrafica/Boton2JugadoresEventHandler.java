2ceecfabb3b0b7d54300ebebcbb594e08016da20
AdriSka <ae.scardapane@gmail.com>
2015-12-02 18:50:47 -0300

    Para 2 jugadores

--- Truco/src/com/interfazgrafica/Boton2JugadoresEventHandler.java
+++ Truco/src/com/interfazgrafica/Boton2JugadoresEventHandler.java
@@ -1,34 +1,3 @@
-
-package com.interfazgrafica;
-
-import com.modelo.Partido;
-
-
-public class Boton2JugadoresEventHandler implements EventHandler<ActionEvent>{
-
-	private Partido partido;
-	private Stage stage;
-	private Scene scene;
-	
-	public Boton2JugadoresEventHandler (Stage stage){
-		this.stage = stage;
-		this.partido = new Partido();
-		Label nuevaLabel = new Label();
-		nuevaLabel.setText("Estan por jugar 2 jugadores");
-		HBox contenedorHorizontal = new HBox (nuevaLabel);
-		contenedorHorizontal.setBackground(new Background(new BackgroundFill(Color.DARKGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-		contenedorHorizontal.setAlignment(Pos.CENTER);
-		this.scene = new Scene(contenedorHorizontal,700,600);
-	}
-	
-	@Override
-	public void handle(ActionEvent actionEvent){
-		this.stage.setScene (scene);
-		this.stage.show();
-	}
-	
-}
-=======
 package com.interfazgrafica;
 
 import com.modelo.Partido;
@@ -39,10 +8,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -52,15 +24,62 @@ public class Boton2JugadoresEventHandler implements EventHandler<ActionEvent>{
 	private Stage stage;
 	private Scene scene;
 	
+	public ImageView generarVisionCartaDorso (){
+		Image dorsoAzul = new Image("file:C:\\Users\\Ska-Ska-Ska\\Desktop\\Cartas Españolas\\CartaDorsoAzul.png");
+		ImageView cartaDorsoAzul = new ImageView (dorsoAzul);
+        cartaDorsoAzul.setFitWidth(70);
+        cartaDorsoAzul.setPreserveRatio(true);
+        cartaDorsoAzul.setSmooth(true);
+        cartaDorsoAzul.setCache(true);
+        return cartaDorsoAzul;
+	}
+	
+	public HBox generarCartasComienzoDeJugador (){
+		ImageView cartaDorso1 = this.generarVisionCartaDorso();
+        ImageView cartaDorso2 = this.generarVisionCartaDorso();
+        ImageView cartaDorso3 = this.generarVisionCartaDorso();
+		HBox cartasJugador = new HBox (cartaDorso1, cartaDorso2, cartaDorso3);
+		cartasJugador.setSpacing(5);
+		cartasJugador.setPadding(new Insets(15));
+		cartasJugador.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+		return cartasJugador;
+	}
+	
+	public HBox generarEspacioVacioVertical (){
+		
+		Image imagenEspacioVacio = new Image("file:C:\\Users\\Ska-Ska-Ska\\Desktop\\Cartas Españolas\\espacioCarta.png");
+		ImageView espacioVacioCarta = new ImageView (imagenEspacioVacio);
+		espacioVacioCarta.setFitWidth(70);
+		espacioVacioCarta.setPreserveRatio(true);
+		espacioVacioCarta.setSmooth(true);
+		espacioVacioCarta.setCache(true);
+		HBox espacioVacio = new HBox (espacioVacioCarta);
+		espacioVacio.setSpacing(5);
+		espacioVacio.setPadding(new Insets(15));
+		espacioVacio.setBackground(new Background(new BackgroundFill(Color.DARKGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+		return espacioVacio;
+	}
+	
 	public Boton2JugadoresEventHandler (Stage stage){
 		this.stage = stage;
 		this.partido = new Partido();
-		Label nuevaLabel = new Label();
-		nuevaLabel.setText("Estan por jugar 2 jugadores");
-		HBox contenedorHorizontal = new HBox (nuevaLabel);
-		contenedorHorizontal.setBackground(new Background(new BackgroundFill(Color.DARKGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-		contenedorHorizontal.setAlignment(Pos.CENTER);
-		this.scene = new Scene(contenedorHorizontal,700,600);
+		
+		HBox tituloPuntajes = new HBox (new Label ("P1"), new Label ("P2"));
+		HBox puntajeJugador1 = new HBox (2);
+		//HBox puntajeJugador2 = new HBox (this.partido.getPuntosUltimoEquipo());
+		//HBox ambosPuntajes = new HBox (puntajeJugador1, puntajeJugador2);
+		//VBox puntajes = new VBox (tituloPuntajes, ambosPuntajes);
+        HBox cartasJugador1EnMano = this.generarCartasComienzoDeJugador();
+        HBox cartasJugador2EnMano = this.generarCartasComienzoDeJugador();
+        HBox cartasJugador1Jugadas = this.generarEspacioVacioVertical();
+        HBox cartasJugador2Jugadas = this.generarEspacioVacioVertical();
+		cartasJugador1EnMano.setAlignment(Pos.TOP_CENTER);
+		cartasJugador2EnMano.setAlignment(Pos.BOTTOM_CENTER);
+		//puntajes.setAlignment(Pos.CENTER_LEFT);
+		
+		VBox campoDeJuego = new VBox (cartasJugador1EnMano, cartasJugador1Jugadas, cartasJugador2Jugadas, cartasJugador2EnMano);
+		
+		this.scene = new Scene(campoDeJuego, 700,600);
 	}
 	
 	@Override
